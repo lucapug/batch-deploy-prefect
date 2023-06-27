e0f8923221459946cae0c40239e29ea5f52bec23
lucapug <lucapug@gmail.com>
2023-06-27 18:11:01 +0200

    correction to score_backfill run_id start end

--- score_backfill.py
+++ score_backfill.py
@@ -8,15 +8,15 @@ from score import ride_duration_prediction
 
 @flow
 def ride_duration_prediction_backfill():
-    start_date = datetime(year=2021, month=3, day=1)
-    end_date = datetime(year=2022, month=4, day=1)
+    start_date = datetime(year=2021, month=4, day=1)
+    end_date = datetime(year=2022, month=5, day=1)
 
     d = start_date
 
     while d <= end_date:
         ride_duration_prediction(
             taxi_type='green',
-            run_id='e1efc53e9bd149078b0c12aeaa6365df',
+            run_id='d038fcff0d5742c58177cf499a456b25',
             run_date=d
         )
 
